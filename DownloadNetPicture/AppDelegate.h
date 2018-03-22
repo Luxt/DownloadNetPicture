cc0274368fe8d386c526a8df189264bc16671e0d
luxiaoting <292327576>
2018-03-22 22:41:41 +0800

    网络下载图片

---
+++ DownloadNetPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadNetPicture
+//
+//  Created by 芦小婷 on 2018/3/22.
+//  Copyright © 2018年 芦小婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownloadNetPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloadNetPicture
+//
+//  Created by 芦小婷 on 2018/3/22.
+//  Copyright © 2018年 芦小婷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
